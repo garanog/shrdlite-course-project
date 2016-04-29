47f736854b870c46c01939229822d872de3cc217
BeardedRoar <joeljoel224@hotmail.com>
2016-04-29 14:07:52 +0200

    wrote comments

--- Graph.ts
+++ Graph.ts
@@ -37,13 +37,9 @@ class SearchResult<Node> {
 
 
 /**
-* A\* search implementation, parameterised by a `Node` type. The code
-* here is just a template; you should rewrite this function
-* entirely. In this template, the code produces a dummy search result
-* which just picks the first possible neighbour.
-*
-* Note that you should not change the API (type) of this function,
-* only its body.
+* A\* search implementation, parameterised by a `Node` type. If no path exists a null-value will
+* be returned. If no solution is found in given timespan for the timeout a error will be thrown.
+*.
 * @param graph The graph on which to perform A\* search.
 * @param start The initial node.
 * @param goal A function that returns true when given a goal node. Used to determine if the algorithm has reached the goal.
@@ -59,30 +55,52 @@ function aStarSearch<Node> (
     timeout : number
 ) : SearchResult<Node> {
 
-    var  initTime = new Date().getTime();
+  // The time the search started, used to check for timeouts.
+  var initTime = new Date().getTime();
 
+  // PriorityQueue contaning all seen paths.
+  // The paths are orded by their costs, which is an combination of the cost off all it's edges
+  // and the heuristic value of the path from it's endnode
   var frontier = new collections.PriorityQueue<SearchResult<Node>>
     (compareSearchResults);
 
+  // Set of all visited nodes, to be able to make sure we don't test the same paths multiple times
+  // and don't get stuck in loops.
   var visited : collections.Set<Node> = new collections.Set<Node>();
   frontier.enqueue(new SearchResult([start], heuristics(start)));
 
+  // Check for each path in the frontier. If the frontier becomes empty, it means there are no more 
+  // reachable nodes and if we have not yet found the goal node there is no solution.
   while (!frontier.isEmpty()) {
   
+    // Check if we have exeeded allowed time, or if a timeout must happen.
     var end = new Date().getTime();
     if (end - initTime > timeout * 1000) {
         throw new Error("Timeout");
     }
+
+    // Pick the current path with lowest cost and heuristic cost, and it's endnode.
     var shortestPath = frontier.dequeue();
     var endNode : Node = shortestPath.path[shortestPath.path.length - 1];
 
+    // Make sure we have not already visited that node.
     if (!visited.contains(endNode)) {
+
+      // If the corrently shortest path reaches the goal, that's the path we're looking for and returns it.
       if (goal(endNode))
         return shortestPath;
 
+      // Mark the node as visited.
       visited.add(endNode);
+
+      // And add all paths that start with the current path and then braches from the endnode to a unvisited
+      // node to the frontier.
       for (var edge of graph.outgoingEdges(endNode)) {
         if (!visited.contains(edge.to)){
+
+          // Create a new searchResult (the representation of a path) by making a deep copy of the current 
+          // path and the new edge, and calculate a new cost. When calculating the new cost, make sure to
+          // update the heuristic from the previous endnode ot that of the new endnode.
           var extendedPath : Array<Node> = extendPath(shortestPath, edge);
           var extendedCost = shortestPath.cost - heuristics(endNode)
             + heuristics(edge.to) + edge.cost;
@@ -95,9 +113,16 @@ function aStarSearch<Node> (
       }
     }
   }
+  // If no soultion is found return a failiure result.
   return null;
 }
 
+/**
+* Creates a combined path from the path of given searchResult and edge by creating a deepcopy.
+* @param path The search result whose path to originate from.
+* @param edge Edge to add to that path.
+* @returns The combined path from the path and the edge.
+*/
 function extendPath<Node>(path : SearchResult<Node>, edge: Edge<Node>) {
   var resultPath : Array<Node> = new Array<Node>(path.path.length);
 
@@ -109,6 +134,13 @@ function extendPath<Node>(path : SearchResult<Node>, edge: Edge<Node>) {
   return resultPath;
 }
 
+/**
+* Compares the cost two searchResults.
+* @param a SearchResult to compare
+* @param b SearchResult to compare
+* @returns Value less then 0 if a is greater then b, a value grater then 0 if b is greater then a
+* and 0 if they're equal.
+*/
 function compareSearchResults<Node>(a : SearchResult<Node>,
                                     b : SearchResult<Node>) {
     return b.cost - a.cost
