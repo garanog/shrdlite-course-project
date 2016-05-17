087f42961b3023910549296fd8e7a55043604aee
garanog <lgerd@student.chalmers.se>
2016-05-17 15:28:41 +0200

    ShrdliteGraph skeleton

---
+++ ShrdliteGraph.ts
@@ -0,0 +1,36 @@
+///<reference path="Graph.ts"/>
+
+class StateNode {
+    constructor(
+        public state : WorldState
+    ) {}
+
+    compareTo(other : StateNode) : number {
+        return 0; //TODO implement
+    }
+
+    toString() : string {
+        return "(" + this.state + ")";
+    }
+}
+
+
+class ShrdliteGraph implements Graph<StateNode> {
+
+    outgoingEdges(node : StateNode) : Edge<StateNode>[] {
+        var outgoing : Edge<StateNode>[] = [];
+
+        // r l p d
+        //TODO: create the up to four nodes, check whether states are possible.
+
+        return outgoing;
+    }
+
+    compareNodes(a : StateNode, b : StateNode) : number {
+        return a.compareTo(b);
+    }
+
+    toString(start? : StateNode, goal? : (n:StateNode) => boolean, path? : StateNode[]) : string {
+        return "Graph."; //TODO
+    }
+}
