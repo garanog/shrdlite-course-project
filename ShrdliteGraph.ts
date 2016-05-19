5de0792cb50a8c7f296b54f9366b215adc4addde
Carly Schneider <cschnei3@binghamton.edu>
2016-05-20 00:03:49 +0200

    ToString and compareTo

--- ShrdliteGraph.ts
+++ ShrdliteGraph.ts
@@ -8,7 +8,12 @@ class StateNode {
     ) {}
 
     compareTo(other : StateNode) : number {
-        return 0; //TODO implement. compare each object in the world, return 1 once difference is found
+    for(var key in this.state.objects) {
+        if(!((other.state.objects[key] === this.state.objects[key]))) {
+            return 1;
+        }
+    }
+    return 0; //TODO implement. compare each object in the world, return 1 once difference is found
     }
 
     toString() : string {
@@ -42,6 +47,22 @@ class ShrdliteGraph implements Graph<StateNode> {
     }
 
     toString(start? : StateNode, goal? : (n:StateNode) => boolean, path? : StateNode[]) : string {
-        return "Graph."; //TODO
+        let retV: string; 
+        if(start){
+            retV += "start: " + start.toString() + "\n";
+        }
+        if( path ){
+            retV += "path: ";
+            for(var node in path){
+                retV += node.toString() + " ";
+            }
+            retV += "\n";
+        }
+        retV += "outgoingEdges: ";
+        for(var node in this.outgoingEdges){
+            retV += (node);
+        }
+        retV += "\n";
+        return retV;
     }
 }
