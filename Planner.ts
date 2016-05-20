710962610657b79f5382e5c82dc84a13944d0cbd
Carlos Martínez Úbeda <charlie.mz@gmail.com>
2016-05-20 12:11:52 +0200

    Correct initialisation of stacks in deepCopy method.

--- Planner.ts
+++ Planner.ts
@@ -80,10 +80,12 @@ module Planner {
 
       var goal = ((node : StateNode) => {
         for (var conjunction of interpretation) { // conjunctions connected by ORs
-
+          console.log("Conjunction: ");
+          console.log(conjunction);
           var conjunctionTrue = true;
           for (var literal of conjunction) { // literals connected by ANDs
             conjunctionTrue = conjunctionTrue && literalHolds(literal, node.state);
+            console.log("ConjunctionTrue: " + conjunctionTrue);
           }
 
           if (conjunctionTrue) {
